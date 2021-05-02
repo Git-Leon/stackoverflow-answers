bfefebff20fb20e0784462298dc87d81e1d9e5a9
git-leon <xleonhunter@gmail.com>
2021-05-02 09:45:03 -0400

    first commit

---
+++ javastreamfilter/src/main/java/com/github/curriculeon/Item.java
@@ -0,0 +1,51 @@
+package com.github.curriculeon;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.management.RuntimeErrorException;
+
+public class Item {
+    private Integer id;
+    private String name;
+    private String description;
+
+    public Item(Integer id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return new ObjectMapper().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ javastreamfilter/src/test/java/com/github/curriculeon/MyObjectTest.java
@@ -0,0 +1,45 @@
+package com.github.curriculeon;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyObjectTest {
+@Test
+public void test() {
+    // given
+    Integer idOfItemToBeRemoved1 = 12345;
+    Integer idOfItemToBeRemoved2 = 56789;
+    Item itemExpectedToBeDeleted1 = new Item(idOfItemToBeRemoved1, "nameOfItem", "descriptionOfItem");
+    Item itemExpectedToBeDeleted2 = new Item(idOfItemToBeRemoved2, "nameOfItem", "descriptionOfItem");
+    Item itemExpectedToBeRetained1 = new Item(34567, "nameOfItem", "descriptionOfItem");
+    Item itemExpectedToBeRetained2 = new Item(98756, "nameOfItem", "descriptionOfItem");
+
+
+    List<Integer> idsOfItemsToRemove = Arrays.asList(
+            idOfItemToBeRemoved1,
+            idOfItemToBeRemoved2);
+
+    List<Item> listOfItems = Arrays.asList(
+            itemExpectedToBeDeleted1,
+            itemExpectedToBeDeleted2,
+            itemExpectedToBeRetained1,
+            itemExpectedToBeRetained2);
+
+    List<Item> expectedList = Arrays.asList(
+            itemExpectedToBeRetained1,
+            itemExpectedToBeRetained2);
+
+    // when
+    List<Item> actualList = listOfItems
+            .stream()
+            .filter(item -> !idsOfItemsToRemove.contains(item.getId()))
+            .collect(Collectors.toList());
+
+    // then
+    Assert.assertEquals(expectedList, actualList);
+}
+}
